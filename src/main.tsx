8d69ac0a4fd64e38e3402d4fdda6f8a40634aae6
idmaksim <i@dmaksim.ru>
2024-10-15 23:36:00 +0300

    Для деплоя

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,20 @@
 import "./index.css";
 
-import { GPU } from "gpu.js";
+// import { GPU } from "gpu.js";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./app/App.tsx";
 
-const gpu = new GPU();
-const add = gpu
-  .createKernel(function (a: number, b: number): number {
-    return a + b;
-  })
-  .setOutput([1]);
+// const gpu = new GPU();
+// const add = gpu
+//   .createKernel(function (a: number, b: number): number {
+//     return a + b;
+//   })
+//   .setOutput([1]);
 
-const result: number[] = add(1, 2) as number[];
-console.log(result[0]);
+// const result: number[] = add(1, 2) as number[];
+// console.log(result[0]);
 async function loadWasm() {
   const wasmModule = await WebAssembly.instantiateStreaming(
     fetch("/wasm/module.wasm"),
--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,14 @@ import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 // @ts-ignore
 import eslintPlugin from "vite-plugin-eslint";
-// @ts-ignore 
-import fs from "fs";
 // @ts-ignore
-import path from "path";
+// import fs from "fs";
+// // @ts-ignore
+// import path from "path";
 // @ts-ignore
-const cert = fs.readFileSync(path.resolve(__dirname, "localhost.pem"));
-// @ts-ignore
-const key = fs.readFileSync(path.resolve(__dirname, "localhost-key.pem"));
+// const cert = fs.readFileSync(path.resolve(__dirname, "localhost.pem"));
+// // @ts-ignore
+// const key = fs.readFileSync(path.resolve(__dirname, "localhost-key.pem"));
 export default defineConfig({
   plugins: [
     react(),
@@ -18,10 +18,13 @@ export default defineConfig({
     }),
   ],
   server: {
-    port: 8080,
-    https: {
-      cert,
-      key,
-    },
+    port: 80,
+    // https: {
+    //   cert,
+    //   key,
+    // },
+  },
+  preview: {
+    port: 80,
   },
 });
